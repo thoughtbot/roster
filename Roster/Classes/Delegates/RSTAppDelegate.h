16015aa924c757e9ec967071678e5ad458507655
Gordon Fontenot <gordon@thoughtbot.com>
2014-04-10 16:43:30 -0400

    Initial Commit

---
+++ Roster/Classes/Delegates/RSTAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RSTAppDelegate.h
+//  Roster
+//
+//  Created by Gordon Fontenot on 4/10/14
+//  Copyright (c) 2014 thoughtbot. All rights reserved.
+//
+
+@interface RSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
